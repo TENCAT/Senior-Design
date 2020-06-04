387c34823173521b4f25a9a77e132d82b44a7b35
Chelsea Carter <chelseatcarter@gmail.com>
2020-06-03 22:31:05 -0400

    Added a button which sends the update command to the server so that data comes in every time it is pressed

--- Android_Code/VehicleApp/app/src/main/java/com/example/vehicleapp/MainActivity.java
+++ Android_Code/VehicleApp/app/src/main/java/com/example/vehicleapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.vehicleapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,23 +11,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 @SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
-    Thread Thread1 = null;
     EditText etIP, etPort;
     TextView tvMessages, speedValTV, directionTV, timeTV, distanceTV;
     String SERVER_IP;
     int SERVER_PORT;
+    DataInputStream din;
+    DataOutputStream dout;
+    Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_main);
         etIP = findViewById(R.id.etIP);
         etPort = findViewById(R.id.etPort);
@@ -34,106 +41,120 @@ public class MainActivity extends AppCompatActivity {
         directionTV = findViewById(R.id.directionTV);
         timeTV = findViewById(R.id.timeTV);
         distanceTV = findViewById(R.id.distanceTV);
-        Button btnConnect = findViewById(R.id.btnConnect);
-        btnConnect.setOnClickListener(new View.OnClickListener() {
+        final Button connectBtn = findViewById(R.id.connectBtn);
+        final Button btnUpdateVals = findViewById(R.id.getUpdatedVals);
+        final Button disconnectBtn = findViewById(R.id.disconnectBtn);
+        disconnectBtn.setVisibility(View.INVISIBLE);
+        btnUpdateVals.setVisibility(View.INVISIBLE);
+
+        btnUpdateVals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    receiveVehicleData();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        disconnectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+//                    dout.close();
+//                    din.close();
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                etIP.setVisibility(View.VISIBLE);
+                etPort.setVisibility(View.VISIBLE);
+                connectBtn.setVisibility(View.VISIBLE);
+                disconnectBtn.setVisibility(View.INVISIBLE);
+                btnUpdateVals.setVisibility(View.INVISIBLE);
+
+                tvMessages.setText("");
+                speedValTV.setText("");
+                directionTV.setText("");
+                timeTV.setText("");
+                distanceTV.setText("");
+            }
+        });
+
+        connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tvMessages.setText("");
                 SERVER_IP = etIP.getText().toString().trim();
                 SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
-                Thread1 = new Thread(new Thread1());
-                Thread1.start();
+
+                try
+                {
+                    socket = new Socket(SERVER_IP,SERVER_PORT);
+                    etIP.setVisibility(View.INVISIBLE);
+                    etPort.setVisibility(View.INVISIBLE);
+                    connectBtn.setVisibility(View.INVISIBLE);
+
+                    disconnectBtn.setVisibility(View.VISIBLE);
+                    tvMessages.setText("Connected to " + SERVER_IP + " at port number: " + SERVER_PORT + "\n");
+                    btnUpdateVals.setVisibility(View.VISIBLE);
+
+                    din=new DataInputStream(socket.getInputStream());
+                    dout=new DataOutputStream(socket.getOutputStream());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    private PrintWriter output;
-    private BufferedReader input;
+    void receiveVehicleData() throws IOException
+    {
+        dout.writeUTF("mobile/update");
+        dout.flush();
 
-    class Thread1 implements Runnable {
-        public void run() {
-            Socket socket;
-            try {
-                socket = new Socket(SERVER_IP, SERVER_PORT);
-                output = new PrintWriter(socket.getOutputStream());
-                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        tvMessages.setText("Connected\n");
-//                        output.write("mobile/update");
-//                        output.flush();
-                        tvMessages.append("Initiated communication with server\n");
+        String message = din.readUTF();
+        String[] messageArray = message.split(" ");
+        for(int i = 0; i < messageArray.length; i++)
+        {
+//            Log.d("Message Array value: ", messageArray[i]);
+            String[] dataType = messageArray[i].split("/");
+            switch (dataType[1]) {
+                case "speed":
+                    int speed = Integer.parseInt(dataType[2]);
+                    //set a textview for speed on the screen with the updated value
+                    speedValTV.setText("Speed: " + speed + " mph");
+                    break;
+                case "steering":
+                    int direction = Integer.parseInt(dataType[2]);
+                    if (direction == 1) {
+                        //display that the steering direction is right
+                        directionTV.setText("Vehicle is traveling towards the right.");
+                    } else if (direction == 2) {
+                        //display that the steering direction is straight
+                        directionTV.setText("Vehicle is traveling straight.");
+                    } else if (direction == 3) {
+                        //display that the steering direction is left
+                        directionTV.setText("Vehicle is traveling towards the left.");
                     }
-                });
-                new Thread(new Thread2()).start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        class Thread2 implements Runnable {
-            @Override
-            public void run() {
-                while (true) {
-                    try
-                    {
-                        final String message = input.readLine();
-                        if(message != null)
-                        {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    //split message by "/"
-                                    String[] messageArray = message.split("/");
-                                    String dataType = messageArray[1];
-                                    switch (dataType) {
-                                        case "speed":
-                                            int speed = Integer.parseInt(messageArray[2]);
-                                            //set a textview for speed on the screen with the updated value
-                                            speedValTV.setText("Speed: " + speed + " mph");
-                                            break;
-                                        case "steering":
-                                            int direction = Integer.parseInt(messageArray[2]);
-                                            if (direction == 1) {
-                                                //display that the steering direction is right
-                                                directionTV.setText("Vehicle is traveling towards the right.");
-                                            } else if (direction == 2) {
-                                                //display that the steering direction is straight
-                                                directionTV.setText("Vehicle is traveling straight.");
-                                            } else if (direction == 3) {
-                                                //display that the steering direction is left
-                                                directionTV.setText("Vehicle is traveling towards the left.");
-                                            }
-                                            break;
-                                        case "time":
-                                            int time = Integer.parseInt(messageArray[2]);
-                                            if (time > 1000) {
-                                                //display that it is currently daytime
-                                                timeTV.setText("It is daytime");
-                                            } else
-                                                //display that it is currently nighttime
-                                                timeTV.setText("It is nighttime");
-                                            break;
-                                        case "distance":
-                                            long distance = Long.parseLong(messageArray[2]);
-                                            //display current distance in cm
-                                            distanceTV.setText("Distance: " + distance + " cm");
-                                            break;
-                                    }
-                                }
-                            });
-                        }
-                        else
-                        {
-                            Thread1 = new Thread(new Thread1());
-                            Thread1.start();
-                            return;
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+                    break;
+                case "DayTime":
+                    int time = Integer.parseInt(dataType[2]);
+                    if (time > 1000) {
+                        //display that it is currently daytime
+                        timeTV.setText("It is daytime");
+                    } else
+                        //display that it is currently nighttime
+                        timeTV.setText("It is nighttime");
+                    break;
+                case "Distance":
+                    long distance = Long.parseLong(dataType[2]);
+                    //display current distance in cm
+                    distanceTV.setText("Distance: " + distance + " cm");
+                    break;
             }
         }
     }
