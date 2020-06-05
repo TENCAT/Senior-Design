c464354506bc6793a68202296078263e40f580ec
RachelBrundage <64284510+RachelBrundage@users.noreply.github.com>
2020-06-05 11:52:43 -0400

    Update testing_vehicle.py
    Added all sensors to test communication between vehicle and Android app

--- Raspberry_pi_Code/python/example/testing_vehicle.py
+++ Raspberry_pi_Code/python/example/testing_vehicle.py
@@ -8,8 +8,19 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-msg = "vehicle/speed/15 "
-client.send(msg.encode('ascii'))
+sensors = {
+    "steering" : 1,
+    "DayTime" : 0,
+    "Distance" : 200,
+    "speed" : 50
+}
+
+# msg = "   vehicle/speed/45 "
+# client.send(msg.encode('ascii'))
+
+for sensor in sensors:
+    msg = "   vehicle/" + sensor + "/" + str(sensors[sensor]) + " "
+    client.send(msg.encode())
 
 while 1:
     print("Received message from server: ", client.recv(4096).decode('ascii'))
