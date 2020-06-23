9e8e1147e4e62c4abe05ba55f2d108080c267882
Chelsea Carter <chelseatcarter@gmail.com>
2020-06-22 20:58:24 -0400

    Fixed issue where keyboard would not be displayed and added black to the colors xml file

--- Android_Code/VehicleApp/app/src/main/java/com/example/vehicleapp/MainActivity.java
+++ Android_Code/VehicleApp/app/src/main/java/com/example/vehicleapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
@@ -25,15 +28,21 @@ public class MainActivity extends AppCompatActivity {
     DataInputStream din;
     DataOutputStream dout;
     Socket socket;
+    final String IPV4_REGEX =
+            "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+    final Pattern IPV4_ADDR_PATTERN = Pattern.compile(IPV4_REGEX);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
         setContentView(R.layout.activity_main);
+
         etIP = findViewById(R.id.etIP);
         etPort = findViewById(R.id.etPort);
         tvMessages = findViewById(R.id.tvMessages);
@@ -44,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         final Button connectBtn = findViewById(R.id.connectBtn);
         final Button btnUpdateVals = findViewById(R.id.getUpdatedVals);
         final Button disconnectBtn = findViewById(R.id.disconnectBtn);
+
+        tvMessages.setVisibility(View.GONE);
         disconnectBtn.setVisibility(View.INVISIBLE);
         btnUpdateVals.setVisibility(View.INVISIBLE);
 
@@ -58,10 +69,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMessages.setVisibility(View.VISIBLE);
+                tvMessages.setText("");
+                SERVER_IP = etIP.getText().toString().trim();
+                //makes sure the IP address entered is valid based on the regex
+                Matcher iPAddrMatcher = IPV4_ADDR_PATTERN.matcher(SERVER_IP);
+                if(!iPAddrMatcher.matches())
+                {
+                    Toast.makeText(getApplicationContext(),"Please enter a valid IP address and port number",
+                            Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
+
+                    try
+                    {
+                        socket = new Socket(SERVER_IP,SERVER_PORT);
+                        etIP.setVisibility(View.INVISIBLE);
+                        etPort.setVisibility(View.INVISIBLE);
+                        connectBtn.setVisibility(View.INVISIBLE);
+
+                        disconnectBtn.setVisibility(View.VISIBLE);
+                        tvMessages.setText("Connected to " + SERVER_IP + " at port number " + SERVER_PORT + "\n");
+                        btnUpdateVals.setVisibility(View.VISIBLE);
+
+                        din=new DataInputStream(socket.getInputStream());
+                        dout=new DataOutputStream(socket.getOutputStream());
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(),"Connection to " + SERVER_IP
+                                + " was unsuccessful", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
         disconnectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
+                    dout.writeUTF("mobile/close");
+                    dout.flush();
                     dout.close();
                     din.close();
                     socket.close();
@@ -72,41 +124,16 @@ public class MainActivity extends AppCompatActivity {
                 etIP.setVisibility(View.VISIBLE);
                 etPort.setVisibility(View.VISIBLE);
                 connectBtn.setVisibility(View.VISIBLE);
+
                 disconnectBtn.setVisibility(View.INVISIBLE);
                 btnUpdateVals.setVisibility(View.INVISIBLE);
+                tvMessages.setVisibility(View.GONE);
 
-                tvMessages.setText("");
+                //clear the screen
                 speedValTV.setText("");
                 directionTV.setText("");
                 timeTV.setText("");
                 distanceTV.setText("");
-            }
-        });
-
-        connectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvMessages.setText("");
-                SERVER_IP = etIP.getText().toString().trim();
-                SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
-
-                try
-                {
-                    socket = new Socket(SERVER_IP,SERVER_PORT);
-                    etIP.setVisibility(View.INVISIBLE);
-                    etPort.setVisibility(View.INVISIBLE);
-                    connectBtn.setVisibility(View.INVISIBLE);
-
-                    disconnectBtn.setVisibility(View.VISIBLE);
-                    tvMessages.setText("Connected to " + SERVER_IP + " at port number: " + SERVER_PORT + "\n");
-                    btnUpdateVals.setVisibility(View.VISIBLE);
-
-                    din=new DataInputStream(socket.getInputStream());
-                    dout=new DataOutputStream(socket.getOutputStream());
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         });
     }
@@ -120,7 +147,6 @@ public class MainActivity extends AppCompatActivity {
         String[] messageArray = message.split(" ");
         for(int i = 0; i < messageArray.length; i++)
         {
-//            Log.d("Message Array value: ", messageArray[i]);
             String[] dataType = messageArray[i].split("/");
             switch (dataType[1]) {
                 case "speed":
@@ -145,10 +171,10 @@ public class MainActivity extends AppCompatActivity {
                     int time = Integer.parseInt(dataType[2]);
                     if (time > 1000) {
                         //display that it is currently daytime
-                        timeTV.setText("It is daytime");
+                        timeTV.setText("It is currently daytime.");
                     } else
                         //display that it is currently nighttime
-                        timeTV.setText("It is nighttime");
+                        timeTV.setText("It is currently nighttime.");
                     break;
                 case "Distance":
                     long distance = Long.parseLong(dataType[2]);
